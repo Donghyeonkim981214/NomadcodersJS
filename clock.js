8908c46fb10b199fafc37d2bbc1da00192ad787e
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-01-24 18:09:03 +0900

    make todolist

--- clock.js
+++ clock.js
@@ -17,7 +17,8 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${year} ${monthName} ${day}(${dayOfWeekName}) ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+  clockTitle.innerText = `${year} ${monthName} ${day}(${dayOfWeekName})
+  ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 
 function init() {
--- name.js
+++ name.js
@@ -24,7 +24,7 @@ function askForName() {
 function paintGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text} It's a perfect day to ~`;
+  greeting.innerText = `Hello!, ${text}`;
 }
 
 function loadName() {
