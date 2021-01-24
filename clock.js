dd766d5a5276bc1e3ab879b6013cea271e314c59
Donghyeonkim981214 <43703346+Donghyeonkim981214@users.noreply.github.com>
2021-01-24 12:27:57 +0900

    add clock

---
+++ clock.js
@@ -0,0 +1,28 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+const dayOfWeekNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function getTime() {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const monthName = monthNames[month];
+  const day = date.getDate();
+  const dayOfWeek = date.getDay();
+  const dayOfWeekName = dayOfWeekNames[dayOfWeek];
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${year}.${monthName}.${day}(${dayOfWeekName}) ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
